2790d586c35799a36796bc7da364b7553ea39438
Arjun K Prasad <arjun.blr1237@gmail.com>
2020-09-20 16:53:50 +0530

    added admin page

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,13 +1,34 @@
+import {status} from './common.js'
+
 window.addEventListener('load', addEvents)
 
 function addEvents(){
-    controlChart()
     let bookings = localStorage.getItem('bookings') || [] 
-    console.log(JSON.parse(bookings))
+    controlChart(JSON.parse(bookings))
 }
 
-function controlChart(){
+function controlChart(bookings){
     var ctx = document.getElementById('myChart').getContext('2d');
+    let cars = JSON.parse(localStorage.getItem('cars') || [])
+    let chartData = bookings.map(x => [new Date(x.carBookedFor).toDateString(), cars.filter(y => y.id === x.carID)[0]?.car?.type])
+    let dates = {}
+    for(let i=0; i<chartData.length; i++){
+        if(dates.hasOwnProperty(chartData[i][0])){
+            if(dates[chartData[i][0]][chartData[i][1]] === undefined){
+                dates[chartData[i][0]] = {}
+                dates[chartData[i][0]][chartData[i][1]] = 1
+            }
+            else{
+                dates[chartData[i][0]][chartData[i][1]]++
+            }
+        }
+        else{
+            dates[chartData[i][0]] = {}
+            dates[chartData[i][0]][chartData[i][1]] = 1
+        }
+    }
+    console.log(dates)
+
     var myChart = new Chart(ctx, {
         type: 'bar',
         data: {
@@ -17,9 +38,21 @@ function controlChart(){
                 data: [12, 19, 3, 5, 2, 3],
                 backgroundColor: 'rgba(255, 99, 132)',
                 borderWidth: 1
+            },{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: 'rgba(155, 99, 232)',
+                borderWidth: 1
             }]
         },
         options: {
+            legend: {
+                display: true,
+                labels: {
+                    fontColor: 'rgb(255, 99)'
+                },
+                position: 'bottom'
+            },
             responsive: true,
             scales: {
                 yAxes: [{
